4cb0307d4edbde2151720ba289e48d37c1cee497
mj <abetterplace@outlook.com>
2023-12-11 17:40:37 -0700

    updated project README file

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
 function listTopics() {
     for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
